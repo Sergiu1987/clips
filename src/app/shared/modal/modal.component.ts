20e6c9f20be05d362f8bf345f44347b7dcf4f0f2
sergiu.siscovschii <sergiu.siscovschii@eu.rsystems.com>
2022-10-06 17:46:59 +0300

    Adding new functional Login/Register form

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor() { }
+  constructor(public modal: ModalService, public el: ElementRef) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
+import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, 
+    SharedModule
   ],
   exports: [
   AuthModalComponent
