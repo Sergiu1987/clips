1d1a92590876e6ccc7c1c39f58220f84d3df7d12
sergiu.siscovschii <sergiu.siscovschii@eu.rsystems.com>
2022-10-15 09:02:16 +0300

    integrate registration COmponnet with Service.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDm4H70u6hfiYYprtjfSRoqe7ruJL95--M',
+    authDomain: 'clips-4886b.firebaseapp.com',
+    projectId: 'clips-4886b',
+    storageBucket: 'clips-4886b.appspot.com',
+    messagingSenderId: '153446984587',
+    appId: '1:153446984587:web:a1a3ec75a318a841209d8e',
+    measurementId: 'G-5DR370T6QB',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDm4H70u6hfiYYprtjfSRoqe7ruJL95--M',
+    authDomain: 'clips-4886b.firebaseapp.com',
+    projectId: 'clips-4886b',
+    storageBucket: 'clips-4886b.appspot.com',
+    messagingSenderId: '153446984587',
+    appId: '1:153446984587:web:a1a3ec75a318a841209d8e',
+    measurementId: 'G-5DR370T6QB',
+  },
 };
 
 /*
